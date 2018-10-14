7f8051ffb22920df0828ccf4f47b02d44eaf6645
pkumar3 <superpransh@gmail.com>
2018-10-14 17:24:27 -0400

    Create checkmate repo

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('css')
+<style type="text/css">
+    #main-wrapper {
+
+    }
+</style>
+@stop
+
+@section('body')
+    @extends('layouts.nav-home')
+    <div id="main-wrapper">
+        <div class="container">
+                
+            <div class="row">
+                <div class="col-12">
+                    <h1 id="heading">Checkmate</h1>
+                </div>
+            </div><!-- /.row -->
+
+        </div><!-- /.container -->
+    </div><!-- /#main-wrapper -->
+@stop
